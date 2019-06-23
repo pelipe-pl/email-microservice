b7f0ad492fbf52ce428c429be48c69b59ad8ce66
pelipe-pl <filipandersen@outlook.com>
2019-06-23 21:39:04 +0200

    User stats scheduler added

--- src/main/java/pl/pelipe/emailmicroservice/controller/web/DashboardController.java
+++ src/main/java/pl/pelipe/emailmicroservice/controller/web/DashboardController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import pl.pelipe.emailmicroservice.dashboard.UserStatsScheduledService;
 
 import java.security.Principal;
 
@@ -11,8 +12,16 @@ import java.security.Principal;
 @Controller
 public class DashboardController {
 
+    private UserStatsScheduledService userStatsService;
+
+    public DashboardController(UserStatsScheduledService userStatsService) {
+        this.userStatsService = userStatsService;
+    }
+
     @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
     public String home(Model model, Principal principal) {
+
+        model.addAttribute("userStatsMap", userStatsService.getUserStatsMap());
 
         return "home";
     }
---
+++ src/main/java/pl/pelipe/emailmicroservice/dashboard/UserStatsScheduledService.java
@@ -0,0 +1,47 @@
+package pl.pelipe.emailmicroservice.dashboard;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import pl.pelipe.emailmicroservice.user.UserRepository;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class UserStatsScheduledService {
+
+    private UserRepository userRepository;
+    private Map<String, Long> userStats = new HashMap<>();
+    private Logger logger = LoggerFactory.getLogger(UserStatsScheduledService.class);
+
+    public UserStatsScheduledService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Scheduled(fixedRate = 60000)
+    private void statsMonitor() {
+
+        Long usersTotal = userRepository.count();
+        Long usersActive = userRepository.countAllByIsActive(true);
+        Long usersRegisteredLast7days = userRepository.countAllByCreatedAtAfter(LocalDateTime.now().minusDays(7));
+
+        userStats.put("usersTotal", usersTotal);
+        userStats.put("usersActive", usersActive);
+        userStats.put("usersRegisteredLast7days", usersRegisteredLast7days);
+
+        logger.info("User stats successfully updated by scheduler (" + this.getClass().getCanonicalName() + ")");
+    }
+
+    public Map<String, Long> getUserStatsMap() {
+        if (userStats.isEmpty()) {
+            Map<String, Long> userStatsEmpty = new HashMap<>();
+            userStatsEmpty.put("usersTotal", 0L);
+            userStatsEmpty.put("usersActive", 0L);
+            userStatsEmpty.put("usersRegisteredLast7days", 0L);
+            return userStatsEmpty;
+        } else return userStats;
+    }
+}
