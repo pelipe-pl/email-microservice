eb6565409e12ac0965f38fbeb63ebc0fa0bbcd9b
pelipe-pl <filipandersen@outlook.com>
2020-03-28 22:04:55 +0100

    User authentication failure logging added.

---
+++ src/main/java/pl/pelipe/emailmicroservice/config/security/AuthenticationFailureHandlerImpl.java
@@ -0,0 +1,31 @@
+package pl.pelipe.emailmicroservice.config.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static pl.pelipe.emailmicroservice.config.keys.Keys.LOG_USER_AUTHENTICATION_FAILED;
+
+@Component
+public class AuthenticationFailureHandlerImpl extends SimpleUrlAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    private Logger logger = LoggerFactory.getLogger(AuthenticationFailureHandlerImpl.class);
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+        logger.warn(String.format(LOG_USER_AUTHENTICATION_FAILED,
+                httpServletRequest.getParameter("username"),
+                e.getMessage(),
+                httpServletRequest.getRemoteAddr()));
+
+        super.onAuthenticationFailure(httpServletRequest, httpServletResponse, e);
+    }
+}
