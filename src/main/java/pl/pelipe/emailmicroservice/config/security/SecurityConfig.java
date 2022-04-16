90d94972c6e1277c596ddd8cd269bcd1f89a65b4
pelipe-pl <filipandersen@outlook.com>
2022-04-16 20:04:41 +0200

    Rest security config update

--- src/main/java/pl/pelipe/emailmicroservice/config/security/SecurityConfig.java
+++ src/main/java/pl/pelipe/emailmicroservice/config/security/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/register", "/resources/**", "/rest/send/**", "/login**", "/forgot-password**")
+                .antMatchers("/register", "/resources/**", "/rest/**", "/login**", "/forgot-password**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
